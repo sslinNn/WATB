278ffccf96f9126a2f74f75a729337d2f360ad80
sslinNn <113080924+sslinNn@users.noreply.github.com>
2023-10-22 21:14:20 +0700

    some pep-8 fixes

--- weather/getLocaion.py
+++ weather/getLocaion.py
@@ -12,7 +12,8 @@ def getLocationFromCoordinates(TOKEN: str, latitude: float, longitude: float):
     api_url_base = f'https://geocode-maps.yandex.ru/1.x/?apikey={TOKEN}&geocode={longitude},{latitude}&format=json'
     response = requests.get(url=api_url_base).text
     location_res = json.loads(response)
-    return location_res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components'][4]['name']
+    return location_res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+        'GeocoderMetaData']['Address']['Components'][4]['name']
 
 def getLocationFromCityName(TOKEN: str, NAME: str):
     api_url_base = f'https://geocode-maps.yandex.ru/1.x/?apikey={TOKEN}&geocode={NAME}&format=json'
@@ -20,20 +21,17 @@ def getLocationFromCityName(TOKEN: str, NAME: str):
     location_res = json.loads(response)
     geo = location_res['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']
     suggest = geo.get('suggest', None)
-    country_name = location_res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components'][0]['name']
+    country_name = location_res['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+        'GeocoderMetaData']['Address']['Components'][0]['name']
     if suggest:
         return re.sub(r'<[^>]+>', '', suggest).title(), country_name, False
     else:
         return geo['request'].title(), country_name, True
 
 
-
-
-
-
-
-
 # with open('locationData.json', 'w', encoding='utf8') as f:
 #     json.dump(location_res.json(), f, ensure_ascii=False)
+
+
 if __name__ == '__main__':
     print(getLocationFromCityName(TOKEN=TOKEN, NAME='масква'))
