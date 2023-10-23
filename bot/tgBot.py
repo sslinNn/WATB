a85bf32694191156b3d32b3d1b9e1045b208d08d
sslinNn <113080924+sslinNn@users.noreply.github.com>
2023-10-24 01:27:08 +0700

    Add some try,excepts. Add weather hendler

--- bot/tgBot.py
+++ bot/tgBot.py
@@ -10,11 +10,13 @@ from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 from weather.getLocaion import getLocationFromCoordinates, getLocationFromCityName
-from User import User
+from bot.handlers.weatherHendler import getWather_hendler
+from statements.User import User
 
 load_dotenv()
 TOKEN = os.getenv('TGBOT_API_KEY')
 TOKENYA = os.getenv('YANDEX_API_KEY')
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 router = Router()
@@ -34,7 +36,6 @@ async def yes(message: types.Message, state: FSMContext):
     await message.answer('Тогда поделись со мной своей геолокацией, пожалуйста!',
                          reply_markup=builder.as_markup(resize_keyboard=True))
 
-
 @dp.message(F.location)
 async def handle_location(message: types.Message, state: FSMContext):
     lon = message.location.longitude
@@ -49,8 +50,6 @@ async def handle_location(message: types.Message, state: FSMContext):
     await state.update_data({'location': userLocation})
     await message.answer(f'Вы находитесь в: {userLocation}?', reply_markup=builder.as_markup(resize_keyboard=True))
 
-
-
 @dp.message(F.text.lower() == 'да')
 async def whereIam(messaage: types.Message, state: FSMContext):
     try:
@@ -58,8 +57,9 @@ async def whereIam(messaage: types.Message, state: FSMContext):
         await messaage.answer(text=f'{data["location"]}')
     except KeyError:
         builder = ReplyKeyboardBuilder()
-        builder.row(types.KeyboardButton(
-            text='Отправить геолокацию', request_location=True), types.KeyboardButton(text='Ввести вручную')
+        builder.row(
+            types.KeyboardButton(text='Отправить геолокацию', request_location=True),
+            types.KeyboardButton(text='Ввести вручную')
         )
         await state.set_state(User.location)
         await messaage.answer('Произошла ошибка, пожалуйста, поделитесь геолокацией снова!',
@@ -69,8 +69,6 @@ async def whereIam(messaage: types.Message, state: FSMContext):
 @dp.message(F.text.lower() == 'ввести вручную')
 async def whereIamManually(message: types.Message):
     await message.answer(text='Хорошо, введите ваш город после /city')
-
-
 
 @dp.message(Command(commands=['city']))
 async def manuallyCity(message: types.Message, state: FSMContext, command: CommandObject):
@@ -91,15 +89,18 @@ async def manuallyCity(message: types.Message, state: FSMContext, command: Comma
     else:
         await message.answer("Пожалуйста, введите ваш город после /city")
 
-
 @dp.message(F.text.lower() == 'какой город у меня указан?')
 async def whichCity(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    await message.answer(text=f'{data["location"]}')
-
+    try:
+        await message.answer(text=f'{data["location"]}')
+    except Exception as ex:
+        logging.exception(ex)
+        await message.answer(text='Я не знаю где вы находитесь!')
 
 async def main():
     bott = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    dp.message.register(getWather_hendler)
     await dp.start_polling(bott)
 
 
--- weather/getWeather.py
+++ weather/getWeather.py
@@ -1,15 +1,19 @@
 import os
 import requests
-import json
 from dotenv import load_dotenv
 
 load_dotenv()
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
-locate = 'Novosibirsk'
 
-api_url_base = f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={locate}&aqi=yes'
 
-weather_res = requests.get(url=api_url_base)
-# print(weather_res.json())
-with open('weatherData.json', 'w') as f:
-    json.dump(weather_res.json(), f)
+def getWeather(locate: str, weather_api_key: str):
+    api_url_base = f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={locate}&aqi=yes'
+    weather_res = requests.get(url=api_url_base)
+    return (f'Местное время: {weather_res.json()["location"]["localtime"]} \n'
+            f'{weather_res.json()["current"]["temp_f"]}°F \n'
+            f'{weather_res.json()["current"]["temp_c"]}°C \n')
+
+
+if __name__ == '__main__':
+    print(getWeather(weather_api_key=WEATHER_API_KEY, locate='Новосибирск'))
+
