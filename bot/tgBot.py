4ee5b3a0b65270c7c7f3bd540dfc5ed7e93afca1
Ilya <ilya40506@mail.ru>
2023-10-25 00:12:12 +0700

    create code input handler and add funcs for it

--- bot/tgBot.py
+++ bot/tgBot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 import sys
+import pandas as pd
 from aiogram import Bot, Dispatcher, types, F, Router
 from aiogram.filters import CommandObject
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
@@ -12,6 +13,13 @@ from dotenv import load_dotenv
 from weather.getLocaion import getLocationFromCoordinates, getLocationFromCityName
 from bot.handlers.weatherHendler import getWather_hendler
 from statements.User import User
+from schedule.selected_schedule_parser import get_daily_schedule, get_weekly_schedule_teacher, get_weekly_schedule_group
+from schedule.all_schedule_parser import getScheduleNHTK_groups, getScheduleNHTK_teachers
+import datetime
+from tabulate import tabulate
+
+
+
 
 load_dotenv()
 TOKEN = os.getenv('TGBOT_API_KEY')
@@ -36,6 +44,90 @@ async def yes(message: types.Message, state: FSMContext):
     await message.answer('Тогда поделись со мной своей геолокацией, пожалуйста!',
                          reply_markup=builder.as_markup(resize_keyboard=True))
 
+
+#ЧЕ ТО Я НЕ СМОГ В ОТДЕЛЬНЫЙ ФАЙЛИК И ПОТОМ СЮДА, АНЛАК
+@dp.message(F.text.lower() == 'в меню')
+async def menu(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(types.KeyboardButton(text='Настройки'), types.KeyboardButton(text='Какой город у меня указан?'), types.KeyboardButton(text='Ввести секретный код'))
+    builder.adjust(2)
+    await message.answer(f'Главное меню', reply_markup=builder.as_markup(resize_keyboard=True))
+
+@dp.message(F.text.lower() == 'ввести секретный код')
+async def enter_code(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(types.KeyboardButton(text='В меню'))
+    await message.answer(f'Введите код после /code', reply_markup=builder.as_markup(resize_keyboard=True))
+
+
+@dp.message(Command(commands=['code']))
+async def answer_to_code(message: types.Message, state: FSMContext, command: CommandObject):
+    if command.args.lower() == 'nhtk' or command.args.lower() == 'нхтк':
+        builder = ReplyKeyboardBuilder()
+        builder.row(types.KeyboardButton(text='Студент'), types.KeyboardButton(text='Преподаватель'), types.KeyboardButton(text='В меню'))
+        builder.adjust(2)
+        await message.answer(f'Вы ввели код: {command.args}, вы явно о чем-то знаете! Если вы хотите получать расписание, пожалуйста, скажите кто вы?', reply_markup=builder.as_markup(resize_keyboard=True))
+
+@dp.message(F.text.lower() == 'студент')
+async def select_group(message: types.Message):
+    df = getScheduleNHTK_groups()
+    groups = df['GroupName'].tolist()
+    print(len(groups))
+    builder = ReplyKeyboardBuilder()
+    for i in groups:
+        builder.add(types.KeyboardButton(text=str(i)))
+    builder.adjust(4)
+    builder.row(types.KeyboardButton(text='В меню'))  # Отдельная строка для "В меню"
+    await message.answer(
+        "Выберите группу из списка или введите вручную",
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+
+@dp.message(F.text.lower() == 'преподаватель')
+async def select_teacher(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(types.KeyboardButton(text='В меню'))  # Отдельная строка для "В меню"
+    await message.answer(
+        "Увы, кнопок нет, но вы можете ввести свою фамилию (например: Иванов. И. И.)",
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+
+@dp.message(Command(commands=['tomorrow_schedule']))
+@dp.message(F.text.lower() == 'расписание на завтра')
+async def tomorrow_schedule(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(types.KeyboardButton(text='В меню'))
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    tomorrow_new = '.'.join(str(tomorrow).split('-')[::-1])
+    df = get_daily_schedule('09.07.11', tomorrow_new)
+
+    df_str = df.to_string(index=False)
+    print(df_str)
+    await message.answer(
+        f'<pre>{df_str}</pre>',
+        parse_mode='HTML',
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+
+@dp.message(Command(commands=['weekly_schedule']))
+@dp.message(F.text.lower() == 'расписание на неделю')
+async def tomorrow_schedule(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.row(types.KeyboardButton(text='В меню'))
+    df = get_weekly_schedule_group('09.07.11')
+    df_str = df.to_string(index=False)
+    print(df_str)
+    await message.answer(
+        f'<pre>{df_str}</pre>',
+        parse_mode='HTML',
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+
 @dp.message(F.location)
 async def handle_location(message: types.Message, state: FSMContext):
     lon = message.location.longitude
@@ -43,7 +135,7 @@ async def handle_location(message: types.Message, state: FSMContext):
     builder = ReplyKeyboardBuilder()
     builder.row(
      types.KeyboardButton(text='Да'),
-     types.KeyboardButton(text='Нет')
+        types.KeyboardButton(text='Нет')
     )
     userLocation = getLocationFromCoordinates(TOKEN=TOKENYA, longitude=lon, latitude=lat)
     await state.set_state(User.location)
@@ -61,9 +153,10 @@ async def whereIam(messaage: types.Message, state: FSMContext):
             types.KeyboardButton(text='Отправить геолокацию', request_location=True),
             types.KeyboardButton(text='Ввести вручную')
         )
+        builder.row(types.KeyboardButton(text='Отправить геолокацию', request_location=True), types.KeyboardButton(text='Ввести вручную'), types.KeyboardButton(text='В меню'))
         await state.set_state(User.location)
         await messaage.answer('Произошла ошибка, пожалуйста, поделитесь геолокацией снова!',
-                              reply_markup=builder.as_markup(resize_keyboard=True))
+                             reply_markup=builder.as_markup(resize_keyboard=True))
 
 
 @dp.message(F.text.lower() == 'ввести вручную')
@@ -75,16 +168,16 @@ async def manuallyCity(message: types.Message, state: FSMContext, command: Comma
     if command.args:
         builder = ReplyKeyboardBuilder()
         builder.row(
-            types.KeyboardButton(text='Какой город у меня указан?')
+            types.KeyboardButton(text='Какой город у меня указан?'), types.KeyboardButton(text='В меню')
         )
         city, country, fixed = getLocationFromCityName(TOKEN=TOKENYA, NAME=str(command.args))
         if fixed:
             await message.answer(f"Установлен город: {city}, страна: {country}",
-                                 reply_markup=builder.as_markup(resize_keyboard=True))
+                             reply_markup=builder.as_markup(resize_keyboard=True))
             await state.update_data({'location': city})
         else:
             await message.answer(f"Вы допустили ошибку, будет выбран город: {city}, страна: {country}",
-                                 reply_markup=builder.as_markup(resize_keyboard=True))
+                             reply_markup=builder.as_markup(resize_keyboard=True))
             await state.update_data({'location': city})
     else:
         await message.answer("Пожалуйста, введите ваш город после /city")
@@ -97,6 +190,14 @@ async def whichCity(message: types.Message, state: FSMContext):
     except Exception as ex:
         logging.exception(ex)
         await message.answer(text='Я не знаю где вы находитесь!')
+    builder = ReplyKeyboardBuilder()
+    builder.row(
+        types.KeyboardButton(text='В меню')
+    )
+    await message.answer(text=f'{data["location"]}',
+                             reply_markup=builder.as_markup(resize_keyboard=True))
+
+
 
 async def main():
     bott = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
