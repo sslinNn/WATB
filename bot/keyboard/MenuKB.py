72cf0057c3506ad9fc3bcda44a0d9710c8c74f18
Ilya <ilya40506@mail.ru>
2023-11-14 23:10:58 +0700

    rework name button

--- bot/keyboard/MenuKB.py
+++ bot/keyboard/MenuKB.py
@@ -3,7 +3,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def getMenuKB():
     kb = ReplyKeyboardBuilder()
     kb.button(text="Настройки")
-    kb.button(text="Погода")
+    kb.button(text="Текущая Погода")
     kb.button(text="Ввести секретный код")
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
