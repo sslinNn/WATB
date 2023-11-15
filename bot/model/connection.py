81dd4305051b0997d321c8e43e0bf071bfde3319
sslinNn <113080924+sslinNn@users.noreply.github.com>
2023-11-15 19:52:32 +0700

    add some requirements in requirements.txt redid querys for SQLAlch

--- bot/model/connection.py
+++ bot/model/connection.py
@@ -2,7 +2,7 @@ import os
 import pymysql.cursors
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-import mysql.connector
+
 
 load_dotenv()
 
@@ -13,30 +13,12 @@ def get_connetion_with_db():
     host_ = os.getenv('host')
     dbname_ = os.getenv('dbname')
     try:
-        connection = create_engine(
+        engine = create_engine(
             f'mysql+mysqlconnector://{user_}:{passwd_}@{host_}/{dbname_}'
         )
-        return connection.connect()
+        return engine
     except Exception as ex:
         return ex
-
-
-# def get_connetion_with_db():
-#     user_ = os.getenv('user')
-#     passwd_ = os.getenv('passwd')
-#     host_ = os.getenv('host')
-#     dbname_ = os.getenv('dbname')
-#     try:
-#         connection = pymysql.connect(
-#             host=host_,
-#             user=user_,
-#             password=passwd_,
-#             database=dbname_,
-#             cursorclass=pymysql.cursors.DictCursor
-#         )
-#         return connection
-#     except Exception as ex:
-#         return ex
 
 
 if __name__ == '__main__':
--- bot/model/querys.py
+++ bot/model/querys.py
@@ -1,23 +1,14 @@
 from bot.model.connection import get_connetion_with_db
-from sqlalchemy.dialects.mysql import insert
+import sqlalchemy as db
 
 def insert_id_and_location_in_db(id_, location_):
     try:
-        with get_connetion_with_db() as conn:
-            meta =
-            sql1 = insert(users).values
-            sql = "INSERT INTO `users` (`id`, `location`) VALUES (%s, %s)"
-            conn.execute(sql, (id_, location_))
+        with get_connetion_with_db().connect() as conn:
+            engine = get_connetion_with_db()
+            meta = db.MetaData()
+            users = db.Table('users', meta, autoload_with=engine)
+            conn.execute(users.insert().values([{'id': id_, 'location': location_}]))
             conn.commit()
     except Exception as ex:
         print(ex)
-
-# def get_location_from_db():
-#     try:
-#         with get_connetion_with_db() as conn:
-#             sql = "INSERT INTO `users` (`id`, `location`) VALUES (%s, %s)"
-#             conn.cursor().execute(sql, (id_, location_))
-#             conn.commit()
-#     except Exception as ex:
-#         print(ex)
 
