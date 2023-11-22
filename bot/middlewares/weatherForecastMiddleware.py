ed240af4a1385f208381095b468e9c637033b1f7
Ilya <ilya40506@mail.ru>
2023-11-23 01:39:56 +0700

    marco reus my favorite marco reus...

--- bot/middlewares/weatherForecastMiddleware.py
+++ bot/middlewares/weatherForecastMiddleware.py
@@ -1,27 +1,26 @@
 from typing import Callable, Awaitable, Dict, Any
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject
+from aiogram.types import TelegramObject, Message
 from bot.model.querys import select_location_from_db
 from weather.graphs import weather_graph
 from weather.getWeather import parse_api
+from bot.keyboard.emoji_control import remove_emojis
 from dotenv import load_dotenv
 import os
 
 
 class weather_forecast(BaseMiddleware):
-    def __int__(self, ID):
-        super().__init__()
-        self.id_ = ID
-        load_dotenv()
-        self.WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
-
-
     async def __call__(self,
-                       handler: Callable[[TelegramObject, Dict[str, Any]],
-                       Awaitable[Any]],
-                       event: TelegramObject,
+                       handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+                       event: Message,
                        data: Dict[str, Any]) -> Any:
-        self.location = select_location_from_db(self.id_)
-        df, sun, date = parse_api(data['location'], self.WEATHER_API_KEY)
-        photo_content = weather_graph(df, sun, date, data['location'])
-        
+        if remove_emojis(event.text).lower() == 'меню':
+            load_dotenv()
+            self.WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+            state = data.get('state')
+            state_data = await state.get_data()
+            location = state_data.get('location', None)
+            df, sun, date = parse_api(location, self.WEATHER_API_KEY)
+            photo = weather_graph(df, sun, date, location)
+            data['photo'] = photo
+            return await handler(event, data)
