8a5e06118841caf87b4cca08d20d9cff6bd3bac6
Ilya <ilya40506@mail.ru>
2023-11-24 02:12:14 +0700

    add "schedule" button

--- bot/keyboard/MenuKB.py
+++ bot/keyboard/MenuKB.py
@@ -1,12 +1,19 @@
+import sqlalchemy
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 import emoji
+from bot.model.querys import Request
 
-def getMenuKB():
+
+def getMenuKB(user_id, request: sqlalchemy.Connection):
+    class_identifier = Request(request).select_code_in_db(user_id)
     gear_emoji = emoji.emojize(":gear:")
     calendar_emoji = emoji.emojize(":calendar:")
     thermometer_emoji = emoji.emojize(":thermometer:")
     clipboard_emoji = emoji.emojize(":clipboard:")
+    schedule_e = emoji.emojize(":spiral_notepad:")
     kb = ReplyKeyboardBuilder()
+    if class_identifier:
+        kb.button(text=f"Расписание{schedule_e}")
     kb.button(text=f"Текущая Погода{thermometer_emoji}")
     kb.button(text=f"Прогноз погоды на сегодня{calendar_emoji}")
     kb.button(text=f"Настройки{gear_emoji}")
