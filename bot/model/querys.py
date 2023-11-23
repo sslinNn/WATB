711e0993caa7a767378e323dd6f43a73589b1de0
Ilya <ilya40506@mail.ru>
2023-11-23 13:52:30 +0700

    create new class for manage DB

--- bot/model/querys.py
+++ bot/model/querys.py
@@ -1,8 +1,42 @@
+import sqlalchemy
+
 from bot.model.connection import get_connetion_with_db
 import sqlalchemy as db
+from datetime import datetime
 
 
-def insert_id_and_location_in_db(id_, location_):
+class Request:
+    def __init__(self, conn: sqlalchemy.Connection):
+        self.conn = conn
+
+
+    async def insert_id_and_location_in_db(self, id_, location_):
+        try:
+            query = db.text("""
+                            INSERT INTO users (id, location)
+                            VALUES (:id, :location)
+                            ON DUPLICATE KEY UPDATE location = :location
+                        """)
+            self.conn.execute(query, {'id': id_, 'location': location_})
+            self.conn.commit()
+        except Exception as ex:
+            print(ex)
+
+
+def select_location_from_db(id_):
+    try:
+        with get_connetion_with_db().connect() as conn:
+            engine = get_connetion_with_db()
+            meta = db.MetaData()
+            users = db.Table('users', meta, autoload_with=engine)
+            query = db.select(users.c.location).where(users.c.id == id_)
+            result = conn.execute(query)
+            return result.fetchone()[0]
+    except Exception as ex:
+        print(ex)
+
+
+def insert_id_and_location_in_db_original(id_, location_):
     try:
         with get_connetion_with_db().connect() as conn:
             engine = get_connetion_with_db()
@@ -19,18 +53,5 @@ def insert_id_and_location_in_db(id_, location_):
         print(ex)
 
 
-def select_location_from_db(id_):
-    try:
-        with get_connetion_with_db().connect() as conn:
-            engine = get_connetion_with_db()
-            meta = db.MetaData()
-            users = db.Table('users', meta, autoload_with=engine)
-            query = db.select(users.c.location).where(users.c.id == id_)
-            result = conn.execute(query)
-            return result.fetchone()[0]
-    except Exception as ex:
-        print(ex)
-
-
 if __name__ == '__main__':
-    print(select_location_from_db(387685744))
+    print(insert_id_and_location_in_db(666, 'hell'))
