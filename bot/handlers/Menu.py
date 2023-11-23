be50fd5a6ee773d530fd38b45dbf3dc0a8745a50
Ilya <ilya40506@mail.ru>
2023-11-24 02:10:44 +0700

    add "schedule" button and functionality for her

--- bot/handlers/Menu.py
+++ bot/handlers/Menu.py
@@ -1,5 +1,7 @@
 import logging
 import os
+
+import sqlalchemy
 from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command, CommandObject
@@ -10,15 +12,15 @@ from weather.graphs import weather_graph
 from bot.schedule.selected_schedule_parser import (get_weekly_schedule_group,
                                                    get_weekly_schedule_teacher,
                                                    get_daily_schedule)
-from bot.statements.states import StartWithUser, Menu, Settings, Secrets
+from bot.statements.states import StartWithUser, Menu, Settings, Secrets, Schedule
 from bot.schedule.converting_df_bez_xyini import df_to_pdf, df_to_xlsx, df_to_png
 from bot.utils.commands import set_commands
 from bot.keyboard.emoji_control import remove_emojis
 from bot.keyboard.MenuKB import getMenuKB
 from bot.keyboard.SettingsKB import getSettingsKB
-from bot.keyboard.SecretKB import getNhtkKB
+from bot.keyboard.SecretKB import getNhtkKB, getScheduleKB
 from io import BytesIO
-
+from bot.model.querys import Request
 
 
 load_dotenv()
@@ -30,19 +32,21 @@ dp = Dispatcher()
 
 
 @dp.message(Menu.menu)
-async def menu(message: types.Message, state: FSMContext):
+async def menu(message: types.Message, state: FSMContext, request: sqlalchemy.Connection):
     await set_commands(bot)
     if message.text.lower() == 'меню':
         await state.set_state(Menu.menuPicker)
-        await message.answer(f'Меню:', reply_markup=getMenuKB())
+        await message.answer(f'Меню:', reply_markup=getMenuKB(message.from_user.id, request))
     else:
         await message.answer('Такого варианта ответа нет!')
 
 
 @dp.message(Command('code'))
-async def secret_code(message: types.Message, command: CommandObject, state: FSMContext):
+async def secret_code(message: types.Message, command: CommandObject, state: FSMContext, request: sqlalchemy.Connection):
     text = command.args
     if text in ['nhtk', 'нхтк']:
+        user_id = message.from_user.id
+        await Request(request).insert_code_in_db(user_id)
         await state.set_state(Secrets.nhtk)
         await message.answer(
             f'Вы ввели код: {text}, вы явно о чем-то знаете!'
@@ -122,7 +126,7 @@ async def daily_schedule(message: types.Message, command: CommandObject):
 
 
 @dp.message(Menu.menuPicker)
-async def menuPicker(message: types.Message, state: FSMContext):
+async def menuPicker(message: types.Message, state: FSMContext, request: sqlalchemy.Connection):
     await set_commands(bot)
     if remove_emojis(message.text.lower()) == 'настройки':
         await state.set_state(Settings.location)
@@ -167,15 +171,17 @@ async def menuPicker(message: types.Message, state: FSMContext):
                     photo_content,
                     filename="weather.png"
                 ),
-                reply_markup=getMenuKB()
+                reply_markup=getMenuKB(message.from_user.id, request)
             )
             await state.set_state(Menu.menuPicker)
         except Exception as ex:
             logging.exception(ex)
             await message.answer(text='Я не знаю где вы находитесь!')
     elif remove_emojis(message.text.lower()) == 'команды':
+        user_id = message.from_user.id
         text = (f'/start - Начало\n'
-                f'/code - Ввести секретный код\n'
+                f'/code - Ввести секретный код')
+        secret_part = (f' (для того чтобы что-то изменить в настройках расписания, вы можете заново ввести код)\n'
                 f'/available_schedule_pdf - Получить доступное расписание в .pdf '
                 f'(после команды нужно ввести номер группы и если вы преподователь - фамилию и инициалы)\n'
                 f'/available_schedule_xlsx - Получить доступное расписание в .xlsx '
@@ -183,6 +189,15 @@ async def menuPicker(message: types.Message, state: FSMContext):
                 f'/daily_schedule - Расписание на определенный день\n'
                 f'(после команды введите сначала группу или фамилию и инициалы преподавателя и дату\n'
                 f'Пример: /daily_schedule 09.07.11 23.11.2023)')
+        code_nhtk = await Request(request).select_code_in_db(user_id)
+        if code_nhtk:
+            text += secret_part
         await message.answer(text)
+    elif remove_emojis(message.text.lower()) == 'расписание':
+        user_id = message.from_user.id
+        await state.set_state(Schedule.schedule)
+        class_identifier = Request(request).select_class_in_db(user_id)
+        text = 'Расписание:'
+        await message.answer(text, reply_markup=getScheduleKB())
     else:
         await message.answer('Такого варианта ответа нет!')
