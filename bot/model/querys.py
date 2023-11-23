32a2d559f909700b9535e17a87cf4233c5a0ba67
Ilya <ilya40506@mail.ru>
2023-11-24 02:13:05 +0700

    add news funcs to manage DB

--- bot/model/querys.py
+++ bot/model/querys.py
@@ -9,7 +9,6 @@ class Request:
     def __init__(self, conn: sqlalchemy.Connection):
         self.conn = conn
 
-
     async def insert_id_and_location_in_db(self, id_, location_):
         try:
             query = db.text("""
@@ -21,6 +20,81 @@ class Request:
             self.conn.commit()
         except Exception as ex:
             print(ex)
+
+    async def insert_role_in_db(self, id_, role):
+        try:
+            query = db.text("""
+            INSERT INTO users (id, role)
+            VALUES (:id, :role)
+            ON DUPLICATE KEY UPDATE role = :role
+            """)
+            self.conn.execute(query, {'id': id_, 'role': role})
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    async def insert_code_in_db(self, id_):
+        try:
+            query = db.text("""
+                                        INSERT INTO users (id, code_nhtk)
+                                        VALUES (:id, 1)
+                                        ON DUPLICATE KEY UPDATE code_nhtk = 1
+                                    """)
+            self.conn.execute(query, {'id': id_})
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    async def insert_group_in_db(self, id_, group):
+        try:
+            query = db.text("""
+                                        INSERT INTO users (id, class_identifier)
+                                        VALUES (:id, :class_identifier)
+                                        ON DUPLICATE KEY UPDATE class_identifier = :class_identifier
+                                    """)
+            self.conn.execute(query, {'id': id_, 'class_identifier': group})
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    async def select_code_in_db(self, id_):
+        try:
+            query = db.text("""
+                                                    SELECT code_nhtk
+                                                    FROM users
+                                                    WHERE id = :id
+                                                """)
+            result = self.conn.execute(query, {'id': id_})
+            print(result)
+            return result.fetchone()[0]
+        except Exception as e:
+            print(e)
+
+    async def select_class_in_db(self, id_):
+        try:
+            query = db.text("""
+                                                    SELECT class_identifier
+                                                    FROM users
+                                                    WHERE id = :id
+                                                """)
+            result = self.conn.execute(query, {'id': id_})
+            print(result)
+            return result.fetchone()[0]
+        except Exception as e:
+            print(e)
+
+    async def select_role_in_db(self, id_):
+        try:
+            query = db.text("""
+                                                    SELECT role
+                                                    FROM users
+                                                    WHERE id = :id
+                                                """)
+            result = self.conn.execute(query, {'id': id_})
+            print(result)
+            return result.fetchone()[0]
+        except Exception as e:
+            print(e)
 
 
 def select_location_from_db(id_):
